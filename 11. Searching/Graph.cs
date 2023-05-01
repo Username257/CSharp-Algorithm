cc9d5d8e927b4a3933dcc7001acedbd82bc1113c
leeuhjean <leeuhjean@naver.com>
2023-05-02 04:00:22 +0900

    feat: 탐색 구현

---
+++ 11. Searching/Graph.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._Searching
+{
+    internal class Graph
+    {
+        /******************************************************
+		 * 그래프 (Graph)
+		 * 
+		 * 정점의 모음과 이 정점을 잇는 간선의 모음의 결합
+		 * 한 노드에서 출발하여 다시 자기 자신의 노드로 돌아오는 순환구조를 가짐
+		 * 간선의 방향성에 따라 단방향 그래프, 양방향 그래프가 있음
+		 * 간선의 가중치에 따라   연결 그래프, 가중치 그래프가 있음
+		 ******************************************************/
+
+
+        // <인접행렬 그래프>
+        // 그래프 내의 각 정점의 인접 관계를 나타내는 행렬
+        // 2차원 배열을 [출발정점, 도착정점] 으로 표현
+        // 장점 : 인접여부 접근이 빠름 O(1)
+        // 단점 : 메모리 사용량이 많음 O(N^2)
+
+        //[시작 정점, 끝 정점] ex) [1, 3] 1정점 -> 3정점 간선
+        bool[,] matrixGraph1 = new bool[5, 5]
+        {   //[0,0]  [0,1] [0,2] [0,3] [0,4]
+            { false, true, true, true, true },
+            { true, false, true, false, true },
+            { true, true, false, false, false },
+            { true, false, false, false, true },
+            { true, true, false, true, false },
+        }; //양방향의 경우 왼상단->우하단 대각선 기준으로 대칭임
+
+        const int INF = int.MaxValue; //무한? 많이 큰 경우
+        int[,] matrixGraph2 = new int[5, 5]
+        {
+            {0, 16, -1, INF, INF },
+            {0, 16, -1, INF, INF },
+            {0, 16, -1, INF, INF },
+            {0, 16, -1, INF, INF },
+            {0, 16, -1, INF, INF },
+        };
+
+        public void Test()
+        {
+            if (matrixGraph1[3, 1])
+            {
+                //갈 수 있음 true
+            }
+        }
+
+
+        // 인접리스트 그래프
+        // 그래프 내의 각 정점의 인접 관계를 표현하는 리스트 O(N)
+        // 인접한 간선만을 리스트에 추가하여 관리 O(N)
+        // 장점 : 메모리 사용량이 적음 
+        // 단점 : 인접여부를 확인하기 위해 리스트 탐색이 필요 
+
+        List<List<int>> listGraph; //연결 그래프
+        List<List<(int, int)>> listGraph2; //가중치 그래프
+        public void CreateGraph()
+        {
+            listGraph = new List<List<int>>();
+            for (int i = 0; i < 5; i++)
+            {
+                listGraph.Add(new List<int>());
+            }
+            listGraph[0].Add(1);
+            listGraph[1].Add(0);
+            listGraph[1].Add(3);
+        }
+
+        
+    }
+}
---
+++ 11. Searching/Searching.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._Searching
+{
+    internal class Searching
+    {
+        // 순차 탐색
+        public static int SequentialSearch<T>(in IList<T> list, in T item) where T : IEquatable<T>
+        { 
+            //in은 입력 전용
+            for(int i = 0; i < list.Count; i++)
+            {
+                if (item.Equals(list[i]))
+                    return i;
+            }
+            return -1;
+        } //자료가 정렬이 안 되어있어도 탐색 가능
+
+        // 이진 탐색
+        public static int BinarySearch<T>(in IList<T> list, in T item) where T : IComparable<T>
+        {
+            int low = 0;
+            int high = list.Count - 1;
+
+            while(low <= high)
+            {
+                int middle = (low + high) / 2; //나누기 연산은 느리다. 곱셈, 플러스는 빠르고 빼기는 더하기로 처리 *0.5f이 차라리 빠름
+                //비트연산자가 제일 빠르다 >> 1, << 1
+                int compare = list[middle].CompareTo(item);
+
+                if (compare < 0)
+                    low = middle + 1;
+                else if (compare > 0)
+                    high = middle - 1;
+                else
+                    return middle;
+            }
+            return -1;
+        } //정렬이 되어있어야 탐색 가능
+        //이진 탐색트리는 논외?
+
+        // DFS BFS (구현방법, 차이점 알아놓기)
+
+        // <깊이 우선 탐색> Depth First Search
+        // 그래프의 분기를 만났을 때 최대한 깊이 내려간 뒤,
+        // 더이상 깊이 갈 곳이 없을 경우 다음 분기를 탐색
+        // 백트래킹(다 해보고 안 되면 되돌아옴) 분할정복
+        public static void DFS(bool[,] graph, int start, out bool[] visited, out int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+                parents[i] = -1;
+            }
+            SearchNode(graph, start, visited, parents);
+        }
+        private static void SearchNode(bool[,] graph, int start, bool[] visited, int[] parents)
+        {
+            visited[start] = true;
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (graph[start, i] && //연결 되어있는 정점이며,
+                     !visited[i])      //방문한 적 없는 정렬
+                {
+                    parents[i] = start;
+                    SearchNode(graph, i, visited, parents);
+                }
+            }
+        }
+
+        // <너비 우선 탐색> Breadth First Search
+        // 그래프의 분기를 만났을 때 모든 분기를 하나씩 저장하고,
+        // 저장한 분기를 한 번씩 거치면서 탐색
+        // 균등하게 한 칸씩 다 가 봄
+        // 동적 계획법
+        public static void BFS(bool[,] graph, int start, out bool[] visited, int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+                parents[i] = -1;
+            }
+
+            Queue<int> bfsQueue = new Queue<int>();
+
+            bfsQueue.Enqueue(start);
+            while(bfsQueue.Count > 0)
+            {
+                int next = bfsQueue.Dequeue();
+                visited[next] = true;
+
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    if (graph[start, i] && //연결 되어있는 정점이며,
+                         !visited[i])      //방문한 적 없는 정렬
+                    {
+                        parents[i] = start;
+                        bfsQueue.Enqueue(i);
+                    }
+                }
+            }
+        }
+    }
+}
